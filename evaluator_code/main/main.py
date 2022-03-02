33b2808beafd5c41cdea69c95f1015946eafa572
victor <lbs.vvieira@ifood3rd.com>
2022-03-01 23:57:00 -0300

    refactor: organize main file

--- evaluator_code/main/main.py
+++ evaluator_code/main/main.py
@@ -1,11 +1,4 @@
-from evaluator_code.model.source_code import SourceCode
 from evaluator_code.service.cyclomatic_complexity import CyclomaticComplexity
-
-# code = SourceCode('/home/victor/TCC/source-code/docs/')
 
 cc = CyclomaticComplexity('/home/victor/TCC/source-code/docs/')
 print(cc.calculate_complexity())
-
-# code.extract_content()
-# code.print_content()
-# print(code.count_code_lines())
