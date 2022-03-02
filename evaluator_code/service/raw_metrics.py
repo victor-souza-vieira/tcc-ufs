4e45c277943e282028623766008ca22826d6d100
victor <lbs.vvieira@ifood3rd.com>
2022-03-02 11:08:13 -0300

    wip: calculate raw metrics add documentation

--- evaluator_code/service/raw_metrics.py
+++ evaluator_code/service/raw_metrics.py
@@ -1,3 +1,4 @@
+import json
 import subprocess as sp
 
 
@@ -6,6 +7,20 @@ class RawMetrics:
         self.path = path
 
     def calculate_raw_metrics(self):
+        """
+        Calculate raw metrics:
+        loc = Total lines of code
+        lloc = Number of logical lines of code
+        sloc = Number of source lines of code
+        comments = Number of python comment lines
+        single_comments = Number of lines which are just comments with no code
+        multi = Number of line which represent multi-line string
+        blank = Number of blank lines or whitespace only
+
+        equation: loc = sloc + blanks + multi + single_comments
+        :return:
+        """
         process_data_string = sp.getoutput('python3 -m radon raw ' + self.path + ' -j')
 
+        process_data_json = json.loads(process_data_string[0:-4])
         return 'a'
