3363d6f4a7f75078c40ac8ba7439aacda4a62ea7
victor <lbs.vvieira@ifood3rd.com>
2022-03-02 12:10:38 -0300

    refactor: add parameter sources_code into cyclomatic_complexity service

--- evaluator_code/main/main.py
+++ evaluator_code/main/main.py
@@ -1,10 +1,17 @@
 from evaluator_code.service.cyclomatic_complexity import CyclomaticComplexity
 from evaluator_code.service.raw_metrics import RawMetrics
+from evaluator_code.model.source_code import SourceCode
 
+path = '/home/victor/TCC/source-code/docs/submissions/'
 
-path = '/home/victor/TCC/source-code/docs/'
-cc = CyclomaticComplexity(path)
+sc1 = SourceCode(path + 'example.py')  # source code
+sc2 = SourceCode(path + 'example2.py')  # source code
+
+scs = [sc1, sc2]  # sources code
+
+cc = CyclomaticComplexity(path, scs)
 print(cc.calculate_complexity())
 
+# ----------------
 rm = RawMetrics(path)
 rm.calculate_raw_metrics()
