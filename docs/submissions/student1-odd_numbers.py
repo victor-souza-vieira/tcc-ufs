b5aaa9c74f6b428909bf064c0aaf1e2257d5cdeb
victor <lbs.vvieira@ifood3rd.com>
2022-03-13 23:25:42 -0300

    samples: remove modularization

--- docs/submissions/student1-odd_numbers.py
+++ docs/submissions/student1-odd_numbers.py
@@ -1,4 +1,19 @@
-def main():
+# def main():
+#     i = 1
+#     j = 1
+#     while True:
+#         if i % 2 == 1:
+#             print(i)
+#             j += 1
+#
+#         i += 1
+#
+#         if j > 10:
+#             break
+
+
+if __name__ == '__main__':
+    # main()
     i = 1
     j = 1
     while True:
@@ -11,6 +26,3 @@ def main():
         if j > 10:
             break
 
-
-if __name__ == '__main__':
-    main()
