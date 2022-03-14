5921c960839363787fe7612228dce57daf3491bf
victor <lbs.vvieira@ifood3rd.com>
2022-03-14 00:28:39 -0300

    feat: add command pattern

---
+++ docs/bubble-sort/submissions/student1-odd_numbers.py
@@ -0,0 +1,28 @@
+# def main():
+#     i = 1
+#     j = 1
+#     while True:
+#         if i % 2 == 1:
+#             print(i)
+#             j += 1
+#
+#         i += 1
+#
+#         if j > 10:
+#             break
+
+
+if __name__ == '__main__':
+    # main()
+    i = 1
+    j = 1
+    while True:
+        if i % 2 == 1:
+            print(i)
+            j += 1
+
+        i += 1
+
+        if j > 10:
+            break
+
--- evaluator_code/main/main.py
+++ evaluator_code/main/main.py
@@ -1,18 +1,3 @@
-from evaluator_code.service.cyclomatic_complexity import CyclomaticComplexity
-from evaluator_code.service.raw_metrics import RawMetrics
-from evaluator_code.model.source_code import SourceCode
+from evaluator_code.command.evaluate_code import main_flow
 
-path = '/home/victor/TCC/source-code/docs/submissions/'
-
-sc1 = SourceCode(path + 'student1-odd_numbers.py')  # source code
-sc2 = SourceCode(path + 'student2-odd_numbers.py')  # source code
-
-scs = [sc1, sc2]  # sources code
-
-# -------------- CYCLOMATIC COMPLEXITY --------------
-cc = CyclomaticComplexity(path, scs)
-cc.calculate_complexity()
-
-# ---------------- RAW METRICS ---------------------
-rm = RawMetrics(path, scs)
-rm.calculate_raw_metrics()
+main_flow()
