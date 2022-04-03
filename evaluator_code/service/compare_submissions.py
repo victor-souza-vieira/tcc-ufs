9528434d26680681443a6d22c023095f21760d9c
victor <lbs.vvieira@ifood3rd.com>
2022-04-02 21:18:51 -0300

    wip: add new metrics

--- evaluator_code/service/compare_submissions.py
+++ evaluator_code/service/compare_submissions.py
@@ -36,9 +36,40 @@ class CompareSubmissions:
 
     def compare_raw_metrics(self):
         self.base.raw_metrics_result = 'Linhas de código: {0}'.format(self.base.raw_metrics['loc'])
+        self.base.raw_metrics_result += '\n\t\tLinhas lógicas de código: {0}'.format(self.base.raw_metrics['lloc'])
+        self.base.raw_metrics_result += '\n\t\tLinhas de código fonte: {0}'.format(self.base.raw_metrics['sloc'])
         for code in self.submissions:
             self.__lines_of_code_metric(code)
             self.__logical_lines_of_code_metric(code)
+            self.__lines_of_source_code_metric(code)
+
+    def __lines_of_source_code_metric(self, code):
+        diff_sloc = code.raw_metrics['sloc'] - self.base.raw_metrics['sloc']
+        alert_sloc = self.alert_config['diff_raw_metrics_sloc']
+        source_lines_of_code = code.raw_metrics['lloc']
+
+        if 0 < diff_sloc > alert_sloc:
+            # Solucao do aluno teve mais linhas de código que a do professor e ultrapassou o limite de alerta
+            code.raw_metrics_result += '\n\t\t\tLinhas de código fonte: {0}; Diferença com a submissão base: {1}; Ultrapassou o limite de alerta de {2}'.format(
+                source_lines_of_code, diff_sloc, alert_sloc)
+            return
+        if 0 < diff_sloc < alert_sloc:
+            # Solucao do aluno teve mais linhas de código que a do professor, porém não ultrapassou o limite de alerta
+            code.raw_metrics_result += '\n\t\t\tLinhas de código fonte: {0}; Diferença com a submissão base: {1};'.format(
+                source_lines_of_code, diff_sloc)
+            return
+        if 0 > diff_sloc < -alert_sloc:
+            # Solucao do aluno teve menos linhas de código que a do professor e ultrapassou o limite de alerta
+            code.raw_metrics_result += '\n\t\t\tLinhas de código fonte: {0}; Menos linhas que a submissão base, porém ultrapassou o limite de alerta de {1}'.format(
+                source_lines_of_code, alert_sloc)
+            return
+        if 0 > diff_sloc > -alert_sloc:
+            # Solucao do aluno teve menos linhas de código que a do professor e não ultrapassou o limite de alerta
+            code.raw_metrics_result += '\n\t\t\tLinhas de código fonte: {0}; Menos linhas que a submissão base e não ultrapassou o limite de alerta'.format(
+                source_lines_of_code)
+            return
+        code.raw_metrics_result += '\n\t\t\tLinhas de código fonte: {0}; Quantidade igual de linhas com a submissão base'.format(
+            source_lines_of_code)
 
     def __logical_lines_of_code_metric(self, code):
         diff_lloc = code.raw_metrics['lloc'] - self.base.raw_metrics['lloc']
--- readme.md
+++ readme.md
@@ -2,10 +2,10 @@
 
 - [X] Verificar o cálculo da complexidade no código media ponderada 39337
 - [ ] Adicionar mais keys para configs de métricas
-- [ ] Estratégia para calcular diferenças entre métricas
-- [ ] Melhorar a forma como é calculada a complexidade ciclomática para arquivos que possuem funções e lógica fora de funcões
+- [X] Estratégia para calcular diferenças entre métricas
+- [X] Melhorar a forma como é calculada a complexidade ciclomática para arquivos que possuem funções e lógica fora de funcões
 - [X] Calcular a complexidade geral do código
 - [X] Enviar as metricas para a CompareSubmissions e utilizá-las
-- [ ] Alunos estão identando códigos com espaço duplo ao invés de utilizar a tecla tab
+- [ ] Alunos estão identando códigos com espaço único ou duplo ao invés de utilizar a tecla tab
 - [ ] A corrida de lesmas faltou a submissão do professor
 - [ ] A C mais ou menos faltou a submissão do professor
